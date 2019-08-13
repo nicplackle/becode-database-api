f436f60cae3a208182358488820d158d35018e0b
nicplackle <nicplackle@gmail.com>
2019-08-13 11:36:03 +0200

    display content of note_app table

--- index.php
+++ index.php
@@ -8,10 +8,12 @@ try{
 // Prepare an insert statement
     $sql = "INSERT INTO note_app (Title, Note) VALUES (:Title, :Note)";
     $stmt = $pdo->prepare($sql);
+    $title = filter_var(trim($_GET['Title']), FILTER_SANITIZE_STRING);
+    $note = filter_var(trim($_POST['Note']), FILTER_SANITIZE_STRING);
 
 // Bind parameters to statement
-    $stmt->bindParam(':Title', $_GET['Title'], PDO::PARAM_STR);
-    $stmt->bindParam(':Note', $_POST['Note'], PDO::PARAM_STR);
+    $stmt->bindParam(':Title', $title, PDO::PARAM_STR);
+    $stmt->bindParam(':Note', $note, PDO::PARAM_STR);
 
 
 // Execute the prepared statement
@@ -20,5 +22,31 @@ try{
     } catch(PDOException $e){
         die("ERROR: Could not prepare/execute query: $sql. " . $e->getMessage());
     }
- 
+
+    try {
+    $listNotes = "SELECT * FROM `note_app`";
+    
+    $result = $pdo->query($listNotes);
+    
+    if ($result->rowCount() > 0) {
+        // output data of each row
+        echo "if statement";
+   
+        while($row = $result->fetch()) {
+            echo "<br>".$row["ID"]."<br>".$row["Title"]."<br>".$row["Note"]."<br>";
+        }
+        unset($result);
+    } else {
+        echo "0 results";
+    }
+    }
+
+    catch(PDOException $e){
+        die("ERROR: Could not prepare/execute query: $listNotes. " . $e->getMessage());
+    }
+    unset($pdo);
+    
+
+//  Return JSON
+
 ?>
